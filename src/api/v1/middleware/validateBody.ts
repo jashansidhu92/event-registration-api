e7164f234db7f5442cd3e1fad0a5c3672b2ab363
jashanpreet <jsdihu55@academic.rrc.ca>
2026-02-22 14:42:25 -0600

    apply validated request body for v1 defaults and stripping

--- src/api/v1/middleware/validateBody.ts
+++ src/api/v1/middleware/validateBody.ts
@@ -4,13 +4,19 @@ import { Schema } from "joi";
 export const validateBody =
   (schema: Schema) =>
   (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body);
+    const { error, value } = schema.validate(req.body, {
+      abortEarly: true,
+      stripUnknown: true,
+      convert: true,
+    });
 
     if (error) {
       return res.status(400).json({
         message: `Validation error: ${error.details[0].message}`,
       });
     }
+
+    req.body = value;
 
     next();
   };
